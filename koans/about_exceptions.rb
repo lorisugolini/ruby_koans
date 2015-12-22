58f8225faed081b84978ecd3cb72014575446570
lorisugolini <loris.ugolini@gmail.com>
2015-12-22 14:31:08 +0100

    WIP about_exceptions

--- koans/about_exceptions.rb
+++ koans/about_exceptions.rb
@@ -6,10 +6,18 @@ class AboutExceptions < Neo::Koan
   end
 
   def test_exceptions_inherit_from_Exception
-    assert_equal __, MySpecialError.ancestors[1]
-    assert_equal __, MySpecialError.ancestors[2]
-    assert_equal __, MySpecialError.ancestors[3]
-    assert_equal __, MySpecialError.ancestors[4]
+    assert_equal RuntimeError, MySpecialError.ancestors[1]
+    assert_equal StandardError, MySpecialError.ancestors[2]
+    assert_equal Exception, MySpecialError.ancestors[3]
+    assert_equal Object, MySpecialError.ancestors[4]
+
+    # UGL aggiunte io
+    assert_equal Kernel, MySpecialError.ancestors[5]  # UGL credevo 'nil', ma quanti sono?
+    assert_equal 7, MySpecialError.ancestors.size
+    assert_equal BasicObject, MySpecialError.ancestors[6]
+    assert_equal nil, MySpecialError.ancestors[7]
+    assert_equal nil, MySpecialError.ancestors[8]
+    assert_equal MySpecialError, MySpecialError.ancestors[0]
   end
 
   def test_rescue_clause
