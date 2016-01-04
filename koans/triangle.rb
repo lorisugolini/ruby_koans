5ffdc812a9cbda4c20cd7c3d8ab61969f1dcbf12
lorisugolini <loris.ugolini@gmail.com>
2016-01-04 14:17:02 +0100

    Completed triangle

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,6 +14,29 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  a, b, c = [a, b, c].sort
+  #puts a, b, c
+  raise TriangleError, "Sides must be numbers greater than zero" if (a <= 0)
+  raise TriangleError, "No two sides can add to be less than or equal to the other side" if (a+b <= c)
+
+  return :equilateral if a == b && b == c
+  return :isosceles if a == b || b == c || a == c
+  return :scalene
+end
+
+# UGL questa è la mia implementazione prima di farmi influenzare dal link di stackoverflow
+# si rompeva qui:
+=begin
+The answers you seek...
+  Exception TriangleError expected, but nothing raised
+
+Please meditate on the following code:
+  /Users/loris/Documents/ruby/ruby_koans/koans/about_triangle_project_2.rb:12:in `test_illegal_triangles_throw_exceptions'
+=end
+
+def my_triangle(a, b, c)
+  raise TriangleError if a <= 0 || b <= 0 || c <= 0
+
   return :equilateral if a == b && b == c
   return :isosceles if a == b || b == c || a == c
   return :scalene
